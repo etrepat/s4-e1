47fc519669b4bb40ba095602affaa21e993d02f0
Estanislau Trepat <estanis@iMac-de-Estanislau-Trepat.local>
2011-01-12 23:30:18 +0100

    aesthetic changes for the CLI. prefer it this way

--- lib/directions.rb
+++ lib/directions.rb
@@ -7,12 +7,8 @@ Bundler.require
 
 require 'httparty'
 
-require File.dirname(__FILE__) + '/directions/app.rb'
-require File.dirname(__FILE__) + '/directions/api.rb'
-
 module Directions
-  def self.get(arguments)
-    Directions::App.new(arguments).run
-  end
 end
 
+require 'directions/app.rb'
+require 'directions/api.rb'
