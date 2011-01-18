3a78bb725b02d81fe13b1488fc777c0d0f163683
Estanislau Trepat <estanis@etrepat.com>
2011-01-19 00:13:53 +0100

    removed custom Measure#inspect method

--- lib/directions/measures.rb
+++ lib/directions/measures.rb
@@ -14,14 +14,6 @@ module Directions
       'unknown'
     end
 
-    # didn't know this but irb complained about not having an inspect method ¿?
-    # it seems that this class inherits from BasicObject which does not have an
-    # "inspect" method defined... well, here it is ;)
-    def inspect
-      inspect_id = "%x" % (object_id * 2)
-      %(#<#{self.class}:0x#{inspect_id} @value=#{value.inspect}, @text=#{text.inspect}>)
-    end
-
     # this way we should be able to compare measures
     def eql?(measure)
       self.class.equal?(measure.class) && @value == measure.value
